a194f7928b1c5095d3b047436c6a9f4c41600fe2
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-07-19 11:11:27 +0300

    Beginning of work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+
+const r = fetch("https://restcountries.com/v3.1/name/peru")
+    .then(response => {
+        return response.json();
+    })
+    .then(countries => {
+        console.log(countries);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+// console.log(r);
