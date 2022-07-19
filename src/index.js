caabfd05f94e20b5a16eec1e6df4fb9a72e3f8a9
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-07-19 12:46:02 +0300

    Beginning of work - 2

--- src/index.js
+++ src/index.js
@@ -9,9 +9,38 @@ const r = fetch("https://restcountries.com/v3.1/name/peru")
     })
     .then(countries => {
         console.log(countries);
+        const markup = createCountriCardMarkup(countries);
+        console.log(markup);
+        countryInfoContainer.innerHTML = markup;
     })
     .catch(error => {
         console.log(error);
     });
 
-// console.log(r);
+// console.log(countries);
+
+//! 1.0.Получаем ссылку на div-контейнер для разметки одной страны:
+const countryInfoContainer = document.querySelector('.country-info');
+// console.log(ImageContainer);
+
+//!   1.2.Добавляем новую разметку в div-контейнер с помощью insertAdjacentHTML:
+// countryInfoContainer.insertAdjacentHTML('beforeend', createCountriCardMarkup);
+
+//!   1.1.Ф-ция, к-рая создает массив с новой разметкой одной страны:
+function createCountriCardMarkup(countries) {
+    return countries
+        .map(({ name: { official }, capital, population, flags: { svg }, languages }) => {
+            return `
+                <div class="card-country">
+                <img class="country-flag"
+                    src="${svg}"
+                    alt="${official}"
+                />
+                <h2 class="card-title">Capital: "${official}"</h2>
+                <p class="card-population">Population: "${population}"</p>
+                <p class="card-languages">languages: "${languages}"</p>
+                </div>
+            `;
+        })
+        .join('');
+}
