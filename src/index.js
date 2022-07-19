3cf6cbba7b9ac5d2b8e3d6020e2460c2d60421ab
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-07-19 14:25:56 +0300

    Beginning of work - 4

--- src/index.js
+++ src/index.js
@@ -3,15 +3,23 @@ import './css/styles.css';
 const DEBOUNCE_DELAY = 300;
 
 
-const r = fetch("https://restcountries.com/v3.1/name/swed")
+const r = fetch("https://restcountries.com/v3.1/name/uni")
     .then(response => {
         return response.json();
     })
     .then(countries => {
         console.log(countries);
-        const markup = createCountriCardMarkup(countries);
-        console.log(markup);
+        // тут надо ставить условие при котором выбирается разная функция для markup
+
+        //! разметка ОДНОЙ страны:
+        const markup = createCountryCardMarkup(countries);
         countryInfoContainer.innerHTML = markup;
+
+        //todo разметки СПИСКА стран:
+        // const markup = createCountriesList(countries);
+        // countriesList.innerHTML = markup;
+
+        console.log(markup);
     })
     .catch(error => {
         console.log(error);
@@ -19,15 +27,18 @@ const r = fetch("https://restcountries.com/v3.1/name/swed")
 
 // console.log(countries);
 
-//! 1.0.Получаем ссылку на div-контейнер для разметки одной страны:
+//! 1.0.Получаем ссылку на div-контейнер для разметки ОДНОЙ страны:
 const countryInfoContainer = document.querySelector('.country-info');
 // console.log(ImageContainer);
+
+//todo 2.0.Получаем ссылку на список для разметки СПИСКА стран:
+const countriesList = document.querySelector('.country-list');
 
 //!   1.2.Добавляем новую разметку в div-контейнер с помощью insertAdjacentHTML:
 // countryInfoContainer.insertAdjacentHTML('beforeend', createCountriCardMarkup);
 
-//!   1.1.Ф-ция, к-рая создает массив с новой разметкой одной страны:
-function createCountriCardMarkup(countries) {
+//!   1.1.Ф-ция, к-рая создает массив с новой разметкой для ОДНОЙ страны:
+function createCountryCardMarkup(countries) {
     return countries
         .map(({ name: { official }, capital, population, flags: { svg }, languages }) => {
             const valuesLanguages = Object.values(languages).join(", ");
@@ -45,6 +56,29 @@ function createCountriCardMarkup(countries) {
                 <p class="card-population">Population: ${population}</p>
                 <p class="card-languages">languages: ${valuesLanguages}</p>
                 </div>
+            `;
+        })
+        .join('');
+}
+
+
+//todo 2.1.Ф-ция, к-рая создает список для разметки СПИСКА стран:
+function createCountriesList(countries) {
+    return countries
+        .map(({ name: { official }, flags: { svg }, languages }) => {
+            return `
+                    <li class="country-item">
+                        <a class="country"
+                        href="">
+                            <img
+                            class="country-image"
+                            width = 30 px
+                            src="${svg}"
+                            alt="${official}"
+                            />
+                        ${official}
+                        </a>
+                    </li>
             `;
         })
         .join('');
